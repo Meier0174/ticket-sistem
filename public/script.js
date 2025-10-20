531aa27d29d19e612f7bfa415768e4bf15f33d5a
User <User@macbooks-MacBook-Pro.local>
2025-10-20 17:13:28 +0300

    update engl last one1

--- public/script.js
+++ public/script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           to: email,
-          subject: "Ihr persönliches Angebot wartet",
+          subject: "Your personal offer is ready",
           text: ` 
           Hello ${name},
 
